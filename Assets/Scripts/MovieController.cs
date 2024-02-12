877966a66c77fb925b3b9d7a420dfc4d6735089f
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2024-02-12 18:54:26 +0100

    ajout de la vidéo d'introduction

---
+++ Assets/Scripts/MovieController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class MovieController : MonoBehaviour {
+    [SerializeField] private VideoPlayer videoPlayer; 
+
+    void Start() {
+        videoPlayer.loopPointReached += EndReached; 
+        Invoke("LoadScene", (float) videoPlayer.clip.length);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+            LoadNextScene(); 
+    }
+
+    void EndReached(VideoPlayer video) {
+        LoadNextScene(); 
+    }
+
+    void LoadNextScene() {
+        SceneManager.LoadScene("Tutorial"); 
+    }
+}
