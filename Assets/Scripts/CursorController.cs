f755d03f8bfc099946e653feb5a25b76aad8422f
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2024-02-12 20:28:52 +0100

    ajout du curseur personnalisé

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CursorController : MonoBehaviour {
+    [SerializeField] private Texture2D cursorTexture;
 
+    void Start() {
+        Cursor.SetCursor(this.cursorTexture, Vector2.zero, CursorMode.Auto);
+    }
 }
